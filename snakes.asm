e799a4346a89fb391301fb1c68ce260ac5773639
Nicole Daniels <ndaniels320gmail.com>
2014-10-13 16:42:34 -0400

    Displays game board with snakes

---
+++ snakes.asm
@@ -0,0 +1,221 @@
+.data
+wall: .asciiz "****************************************************************",
+	      "*                                                              *",
+	      "*                                                              *",
+	      "*                                        ***********************",
+	      "*                                        *                      ",
+	      "*                                        * *********************",
+	      "*                                        * *                   *",
+ 	      "*                                        * *                   *",
+ 	      "*                                        * *                   *",
+	      "*                                                              *",
+	      "*                                                              *",
+	      "*                                                              *",
+	      "*                                                              *",
+	      "*                                                              *",
+	      "*                                                              *",
+ 	      "*                        ****************                      *",
+ 	      "*                        *              *                      *",
+	      "*                        *  **********  *                      *",
+	      "*                        *  *        *  *                      *",
+	      "*                        *  *        *  *                      *",
+	      "*                                                              *",
+	      "*                                                              *",
+	      "*                                                              *",
+ 	      "*                                                              *",
+ 	      "*                                                              *",
+	      "*        ******************                                    *",
+	      "*        *                *                                    *",
+	      "*        *   *******      *                                    *",
+	      "*        *   *     *      *                                    *",
+	      "*        *   *******      *                                    *",
+	      "*                         *                                    *",
+ 	      "*               ***********                                    *",
+ 	      "*                                                              *",
+	      "*                                                              *",
+	      "*                                                              *",
+	      "*                                                              *",
+	      "*                                                              *",
+	      "*                                                              *",
+	      "*                                                              *",
+ 	      "*                                                              *",
+ 	      "*                                ***************               *",
+	      "*                                              *               *",
+	      "*                                ************* *               *",
+	      "*                                            * *               *",
+              "*                                                              *",
+	      "*                                                              *",
+	      "*                                                              *",
+ 	      "*              *	*                                             *",
+ 	      "*              *	*                                             *",
+	      "*              *	*                                             *",
+	      "****************	*                                             *",
+	      "                 *               *           *                 *",
+	      "******************               *           *                 *",
+	      "*                                *           *                 *",
+	      "*                                *           *                 *",
+ 	      "*                                *************                 *",
+ 	      "*                                                              *",
+	      "*                                                              *",
+	      "*                                                              *",
+	      "*                                                              *",
+	      "*                                                              *",
+	      "*                                                              *",
+              "*                                                              *",
+ 	      "****************************************************************",
+snakeBuffer: .byte 4,31,5,31,6,31,7,31,8,31,9,31,10,31,11,31
+snakeBufferExt: .space 512
+.text
+la $t4,wall
+#byte array
+#512 bytes
+assembleBoard:
+lb $t5,0($t4)
+beq $t5,0x2a,foundAsterik
+beq $t5,0,nextLine
+addi $t6,$t6,1
+addi $t4,$t4,1
+j assembleBoard
+
+foundAsterik:
+move $a0,$t6
+move $a1,$t7
+li $a2,1
+jal _setLED
+addi $t4,$t4,1
+addi $t6,$t6,1
+j assembleBoard
+
+nextLine:
+addi $v0,$v0,1
+beq $v0,64,end
+addi $t7,$t7,1
+li $t6,0
+addi $t4,$t4,1
+j assembleBoard
+
+setSnakes:
+
+end:
+li $t4,0 #counter for frog placements
+li $t5,0 #stores random x value
+li $t6,0 #stores random y value
+li $t7,0
+
+placeFrogs:
+bgt $t4,32,setSnake #32 possible snakes
+addi $t4,$t4,1
+li $v0,42
+li $a0,5
+li $a1,63
+syscall
+move $t5,$a0
+li $v0,42
+li $a0,5
+li $a1,63
+syscall
+move $t6,$a0
+move $a0,$t5
+move $a1,$t6
+li $a2,3
+jal _getLED
+beq $v0,0,validPosition
+j placeFrogs
+
+validPosition:
+jal _setLED
+addi $t7,$t7,1 #counts number of frogs on board
+j placeFrogs
+
+setSnake:
+bgt $t5,10,exit
+li $t5,4 #x-value of snake
+li $t6,31 #y-value of snake
+li $a2,2
+move $a0,$t5
+move $a1,$t6
+jal _setLED
+addi $t5,$t5,1
+j setSnake
+
+exit:
+li $v0,10
+syscall
+
+_queue_insert:
+
+_queue_remove:
+
+_queue_peek_end:
+
+
+#jal gameLoop
+
+# _setLED and _getLED functions for Keypad and LED Display Simulator (64x64)
+#
+# These functions may be used in your CS/CoE 0447 Project 1.
+# They provide a convenient interface to the Keypad and LED Display Simulator
+# extension (64x64) in MARS 4.4-Pitt.1.  For arguments and return values,
+# read the comments above each; call them like any other MIPS function.
+#
+# If you're really interested, look through the code to show yourself
+# how it works, or even practice writing these yourself!  You know
+# all the pieces; try fitting them together!
+
+
+	# void _setLED(int x, int y, int color)
+	#   sets the LED at (x,y) to color
+	#   color: 0=off, 1=red, 2=yellow, 3=green
+	#
+	# arguments: $a0 is x, $a1 is y, $a2 is color
+	# trashes:   $t0-$t3
+	# returns:   none
+	#
+_setLED:
+	# byte offset into display = y * 16 bytes + (x / 4)
+	sll	$t0,$a1,4      # y * 16 bytes
+	srl	$t1,$a0,2      # x / 4
+	add	$t0,$t0,$t1    # byte offset into display
+	li	$t2,0xffff0008 # base address of LED display
+	add	$t0,$t2,$t0    # address of byte with the LED
+	# now, compute led position in the byte and the mask for it
+	andi	$t1,$a0,0x3    # remainder is led position in byte
+	neg	$t1,$t1        # negate position for subtraction
+	addi	$t1,$t1,3      # bit positions in reverse order
+	sll	$t1,$t1,1      # led is 2 bits
+	# compute two masks: one to clear field, one to set new color
+	li	$t2,3		
+	sllv	$t2,$t2,$t1
+	not	$t2,$t2        # bit mask for clearing current color
+	sllv	$t1,$a2,$t1    # bit mask for setting color
+	# get current LED value, set the new field, store it back to LED
+	lbu	$t3,0($t0)     # read current LED value	
+	and	$t3,$t3,$t2    # clear the field for the color
+	or	$t3,$t3,$t1    # set color field
+	sb	$t3,0($t0)     # update display
+	jr	$ra
+	
+	# int _getLED(int x, int y)
+	#   returns the value of the LED at position (x,y)
+	#
+	#  arguments: $a0 holds x, $a1 holds y
+	#  trashes:   $t0-$t2
+	#  returns:   $v0 holds the value of the LED (0, 1, 2 or 3)
+	#
+_getLED:
+	# byte offset into display = y * 16 bytes + (x / 4)
+	sll  $t0,$a1,4      # y * 16 bytes
+	srl  $t1,$a0,2      # x / 4
+	add  $t0,$t0,$t1    # byte offset into display
+	la   $t2,0xffff0008
+	add  $t0,$t2,$t0    # address of byte with the LED
+	# now, compute bit position in the byte and the mask for it
+	andi $t1,$a0,0x3    # remainder is bit position in byte
+	neg  $t1,$t1        # negate position for subtraction
+	addi $t1,$t1,3      # bit positions in reverse order
+    	sll  $t1,$t1,1      # led is 2 bits
+	# load LED value, get the desired bit in the loaded byte
+	lbu  $t2,0($t0)
+	srlv $t2,$t2,$t1    # shift LED value to lsb position
+	andi $v0,$t2,0x3    # mask off any remaining upper bits
+	jr   $ra
